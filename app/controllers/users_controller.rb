600c06f91070526bdefddadfbcd0de1df8cbab2d
松岡竜太郎 <aaaaiiiine@gmail.com>
2019-10-02 17:31:27 +0900

    Correct mistake

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
 
-		def index
-			@users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
-			respond_to do |format|
-				format.html
-				format.json
+    def index
+      @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+      respond_to do |format|
+        format.html
+        format.json
 			end
-		end
+    end
 
 
     def edit

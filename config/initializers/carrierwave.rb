460b14f16471dc1b354892c148480654311bc669
松岡竜太郎 <>
2019-10-15 16:49:45 +0900

    final setting

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'chattryu'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ap-northeast-1'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chattryu'
 end
